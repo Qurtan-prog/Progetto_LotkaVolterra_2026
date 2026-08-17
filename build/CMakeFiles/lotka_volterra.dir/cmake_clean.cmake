71eeebc3dbb0a1cccfba346cbcf73f46921f7c51
Qurtan-prog <artan.shala@studio.unibo.it>
2026-08-17 21:27:25 +0200

    Finalmente finito il plotter, non so se funziona perche' su debian ho problemi con il display grafico ma vediamo

---
+++ build/CMakeFiles/lotka_volterra.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lotka_volterra.dir/link.d"
+  "CMakeFiles/lotka_volterra.dir/Plotter.cpp.o"
+  "CMakeFiles/lotka_volterra.dir/Plotter.cpp.o.d"
+  "CMakeFiles/lotka_volterra.dir/input.cpp.o"
+  "CMakeFiles/lotka_volterra.dir/input.cpp.o.d"
+  "CMakeFiles/lotka_volterra.dir/main.cpp.o"
+  "CMakeFiles/lotka_volterra.dir/main.cpp.o.d"
+  "CMakeFiles/lotka_volterra.dir/simulation.cpp.o"
+  "CMakeFiles/lotka_volterra.dir/simulation.cpp.o.d"
+  "lotka_volterra"
+  "lotka_volterra.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lotka_volterra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
