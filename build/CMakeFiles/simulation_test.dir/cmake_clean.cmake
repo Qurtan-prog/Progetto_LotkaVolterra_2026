99e5cffb67c1f7dff7b1768b250f972014c4c79c
Qurtan-prog <artan.shala@studio.unibo.it>
2026-08-25 15:29:05 +0200

    aggiunti e/o cambiati i file del main con quelli di artan-branch

---
+++ build/CMakeFiles/simulation_test.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simulation_test.dir/link.d"
+  "CMakeFiles/simulation_test.dir/input.cpp.o"
+  "CMakeFiles/simulation_test.dir/input.cpp.o.d"
+  "CMakeFiles/simulation_test.dir/simulation.cpp.o"
+  "CMakeFiles/simulation_test.dir/simulation.cpp.o.d"
+  "CMakeFiles/simulation_test.dir/simulation_test.cpp.o"
+  "CMakeFiles/simulation_test.dir/simulation_test.cpp.o.d"
+  "simulation_test"
+  "simulation_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simulation_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
