5ce2f9e4c4fea9addc7ece012022651708a30e36
AnitaTonin0 <anitatonin@icloud.com>
2026-08-26 16:08:58 +0200

    Ho rimosso la cartella build che hai creato perche era legato al percorso su git  equindi io non potevo compilare, cartella build dal repository generata localmente

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/artan/Progetto_prog
-# Build directory: /home/artan/Progetto_prog/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(simulation_test "/home/artan/Progetto_prog/build/simulation_test")
-set_tests_properties(simulation_test PROPERTIES  _BACKTRACE_TRIPLES "/home/artan/Progetto_prog/CMakeLists.txt;35;add_test;/home/artan/Progetto_prog/CMakeLists.txt;0;")
